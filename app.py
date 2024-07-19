d740448976da37568d4961e48bb5b2d4cd7b669d
Zaki <zakinabdul.tech@gmail.com>
2024-07-19 08:46:50 +0530

    removed .venv from tracking

--- app.py
+++ app.py
@@ -1,8 +1,40 @@
+# importing necessory libraries
 import streamlit as st
 import google.generativeai as genai
 import os
 from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_groq import ChatGroq
+
+
+
 load_dotenv()
+chat = ChatGroq(
+    temperature=0,
+    model="llama3-70b-8192",
+    api_key=os.getenv("GROQ_API") # Optional if not set as an environment variable
+)
+human = st.chat_input("Enter your mesg")
+#system = "You are a data analyst specialist."
+#system = st.text_input("Exaplin who is the AI")
+#human = "{text}"
+#prompt = ChatPromptTemplate.from_messages([("system", system), ("human", human)])
+if human:
+    with st.chat_message("user"):
+        st.write(human)
+    with st.chat_message("assistant", avatar="🎃"):
+        st.write(human)
+#prompt = input("Enter your prompt:")
+#sdf = SmartDataframe(df, config={"llm": chat})
+#chain = prompt | chat
+#response = st.text_area("Enter your Prompt")
+#res = chain.invoke({"text": response})
+#res = sdf.chat(prompt)
+#if st.button("Generate"):
+ #   with st.spinner("Wait a second........"):
+  #      st.write(res.text)
+#else:
+ #   print("Problem")
 genai.configure(api_key=os.getenv("Google_API_KEY"))
 model = genai.GenerativeModel('gemini-1.5-pro')
 st.title("Our Own Gemini😎")
